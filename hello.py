5f82923c5d70bba9843280975cf9a1c14a8a1af2
Maxmilian626 <maxmilian.wei@students.olin.edu>
2016-03-28 21:57:52 -0400

    I tried.  I got some parts working, then got really stuck and confused.

--- hello.py
+++ hello.py
@@ -2,12 +2,21 @@
 Simple "Hello, World" application using Flask
 """
 
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def submit_form():
+	return render_template('index.html')
+
+
+@app.route('/login', methods=['POST'])
+def submit_form_post():
+	name = request.form('name')
+	age = request.form('age')
+	ninja = request.form('FavNinja')
+	return render_template('index.html', name=name, age=age, ninja='Patrick Huston')
 
 if __name__ == '__main__':
     app.run()
